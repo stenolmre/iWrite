bdb2767d1d99855b943e97d0a530c8430183907e
Sten Olmre <stenolmre@icloud.com>
2021-01-22 12:04:37 +0200

    test

--- pages/[id].js
+++ pages/[id].js
@@ -1,37 +1,38 @@
 import React, { Fragment, useEffect } from 'react'
 import Head from './../utils/head'
-import { useRouter } from 'next/router'
-import Cookies from 'js-cookie'
+import axios from 'axios'
 
 import Layout from './../components/layout'
 import Loader from './../components/loader'
 import Poem from './../components/poem'
 
-import { usePoemState, usePoemDispatch } from './../context/poem'
-import { getPoem } from './../actions/poem'
+import { usePoemDispatch } from './../context/poem'
 
-const MyComponent = () => {
-  const { query } = useRouter()
-
-  const { poem, loading } = usePoemState()
+const PoemPage = ({ poem }) => {
   const dispatchPoem = usePoemDispatch()
 
-  useEffect(() => {
-    getPoem(dispatchPoem, query.id)
-  }, [dispatchPoem, query.id])
-
   return <Fragment>
-    <Head title="iWrite" url="https://www.iwrite.im/"/>
+    <Head title={`iWrite - ${poem.name}`} url="https://www.iwrite.im/"/>
     <Layout>
       <div className="poems">
         {
-          loading
+          !poem
             ? <Loader />
-            : poem && <Poem key={poem._id} id={poem._id} name={poem.name} date={new Date(poem.createdAt).toLocaleDateString()} text={poem.text} dispatchPoem={dispatchPoem} linkName={el.name.toLowerCase().replace(' ', '-')}/>
+            : <Poem key={poem._id} id={poem._id} name={poem.name} date={new Date(poem.createdAt).toLocaleDateString()} text={poem.text} dispatchPoem={dispatchPoem} linkName={poem.name.toLowerCase().replace(' ', '-')}/>
         }
       </div>
     </Layout>
   </Fragment>
 }
 
-export default MyComponent
+PoemPage.getInitialProps = async ctx => {
+  const { id } = await ctx.query
+
+  const { data } = !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+    ? await axios.get(`http://localhost:3000/api/poem/get?id=${ id }`)
+    : await axios.get(`https://stenolmre.com/api/poem/get?id=${ id }`)
+
+  return { poem: data }
+}
+
+export default PoemPage
