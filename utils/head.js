9062dfec30287f219eb4d2d61543f7abd4aa3223
Sten Olmre <stenolmre@icloud.com>
2021-01-23 19:53:55 +0200

    test

--- utils/head.js
+++ utils/head.js
@@ -7,15 +7,15 @@ export default function MetaTags({ title, url }) {
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"/>
     <meta name="description" content="iWrite purpose is to offer you a flight across the space created in one's mind."/>
-    <meta name="image" content="share_image.jpg"/>
+    <meta name="image" content="https://res.cloudinary.com/djz69vbsq/image/upload/v1611424373/iWrite/share_img_uylypc.jpg"/>
     <meta name="keywords" content="Poetry, Novels, Toska, Feelings, Words"/>
     <meta property="og:title" content={title}/>
     <meta property="og:description" content="iWrite purpose is to offer you a flight across the space created in one's mind."/>
-    <meta property="og:image" content="share_image.jpg"/>
+    <meta property="og:image" content="https://res.cloudinary.com/djz69vbsq/image/upload/v1611424373/iWrite/share_img_uylypc.jpg"/>
     <meta property="og:url" content={url}/>
     <meta name="twitter:title" content={title}/>
     <meta name="twitter:description" content="iWrite purpose is to offer you a flight across the space created in one's mind."/>
-    <meta name="twitter:image" content="share_image.jpg"/>
+    <meta name="twitter:image" content="https://res.cloudinary.com/djz69vbsq/image/upload/v1611424373/iWrite/share_img_uylypc.jpg"/>
     <meta name="twitter:card" content="summary_large_image"/>
   </Head>
 }
